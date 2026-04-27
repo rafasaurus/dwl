38b8351c13c109608df4d837fc4d9f6edcf7d75f
rafasaurus <rafaelkgrigoryan@gmail.com>
2026-04-27 14:32:45 +0400

    Change terminal to env variable

--- config.h
+++ config.h
@@ -157,7 +157,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *termcmd[] = { "alacritty", NULL };
+static const char *termcmd[] = { "$TERMINAL", NULL };
 static const char *menucmd[] = { "wmenu-run", NULL };
 static const char *launchcmd[] = { "fuzzel", NULL };
 static const char *brightup[] = { "brightnessctl", "set", "10%+", NULL };
